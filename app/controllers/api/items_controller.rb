dba7d8d596ace63728ff173ef585fd2bd4d0ba4a
dpg5000 <dgoussev@gmail.com>
2015-09-10 21:45:59 -0700

    add destroy

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -12,6 +12,17 @@ class Api::ItemsController < ApiController
     end
   end
 
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+
 private
 
   def item_params
